7faf688a0ad4bb39e5ac9e3b6e1f274449e75d16
a-wong-8 <awong88@berkeley.edu>
2023-07-04 14:24:28 -0700

    added sign up to navbar

--- frontend/src/components/LoginModal/LoginModal.js
+++ frontend/src/components/LoginModal/LoginModal.js
@@ -1,15 +1,25 @@
 import { useState } from "react";
 import {Modal} from "../context/Modal";
 import { SignInPage } from "../NavBar/UserAuthPages/SignInPage/SignInPage";
+import { SignUpPage } from "../NavBar/UserAuthPages/SignUpPage/SignUpPage";
 
 export default function LoginFormModal () {
 
     const[showModal, setShowModal] = useState(false);
 
+    const [showSignUpModal, setShowSignUpModal] = useState(false);
+
     return (
         <>
-            <a onClick={()=>setShowModal(true)}> Sign in </a>
-            
+            <a onClick={()=>{setShowSignUpModal(true);}}> Sign up </a>
+
+            <a id="signin" onClick={()=>setShowModal(true)}> Sign in </a>
+
+            {showSignUpModal && (
+            <Modal onClose={()=> setShowSignUpModal(false)} >
+                <SignUpPage/>
+            </Modal>)}
+
             {showModal && (
             <Modal onClose={()=> setShowModal(false)}>
                 <SignInPage/>
