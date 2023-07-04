75f568f33ca5be7d473753f2c8a5f3709c298298
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-07-04 13:47:44 -0700

    Connect splash page buttons to associated modals

--- frontend/src/components/MainPage/Splash/Splash.jsx
+++ frontend/src/components/MainPage/Splash/Splash.jsx
@@ -1,6 +1,20 @@
 import './Splash.css';
+import { useState } from 'react';
+import { Modal } from "../../context/Modal"
+import { SignInPage } from "../../NavBar/UserAuthPages/SignInPage/SignInPage"
+import { SignUpPage } from "../../NavBar/UserAuthPages/SignUpPage/SignUpPage"
 
 export default function Splash () {
+    const [showLoginModal, setShowLoginModal] = useState(false);
+    const [showSignUpModal, setShowSignUpModal] = useState(false);
+    const handleModalClose = (e) => {
+        setShowLoginModal(false);
+        setShowSignUpModal(false);
+    }
+    const handleModalOpen = async (e) => {
+        setShowLoginModal(true);
+        setShowSignUpModal(true);
+    }
 
     return (
         <div className='splash-background'>
@@ -13,8 +27,14 @@ export default function Splash () {
                     {/* <br/> Don't trip, we've got you covered. */}
                 </p>
                 <ul className='splash-buttons'>
-                    <button>Sign In</button>
-                    <button>Sign Up</button>
+                    <button onClick={() => {setShowSignUpModal(true);}}>Sign Up</button>
+                    <button onClick={() => { setShowLoginModal(true)}}>Login</button>
+                    {showSignUpModal && (<Modal onClose={handleModalClose} >
+                            <SignUpPage/>
+                        </Modal>)}
+                    {showLoginModal && (<Modal onClose={handleModalClose} >
+                            <SignInPage/>
+                        </Modal>)}
                     <button>Demo</button>
                 </ul>
             </article>
