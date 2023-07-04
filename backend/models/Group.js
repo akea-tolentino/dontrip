1339abfc6fabe23a815c4002096edb20f434dc51
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-07-04 12:57:23 -0700

    working on group routes

--- backend/models/Group.js
+++ backend/models/Group.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const groupSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
     members: {
         type: Number,
         required: true
--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -9,22 +9,117 @@ const validateGroupInput = require('../../validations/group');
 
 router.get('/:groupId/users/:userId', async(req, res, next) => {
     //group show route for a user
+    let user;
+    try {
+        user = await User.findById(req.params.userId);
+    } catch(err) {
+        const error = new Error('User not found');
+        error.statusCode = 404;
+        error.errors = {message: "No user found with that id"};
+        return next(error);
+    }
+    try {
+        const group = await Group.findById(req.params.groupId)
+                         .populate("month location experience");
+        return res.json(group);
+    } catch(err) {
+        const error = new Error('Group not found');
+        error.statusCode = 404;
+        error.errors = {message: "No Group found with that id"};
+        return next(error);
+    }
 });
 
 router.patch('/:groupId/users/:userId', validateGroupInput, async(req, res, next) => {
     //group update route for a user
+    let user;
+    try {
+        user = await User.findById(req.params.userId);
+    } catch(err) {
+        const error = new Error('User not found');
+        error.statusCode = 404;
+        error.errors = {message: "No user found with that id"};
+        return next(error);
+    }
+    try {
+        const group = await Group.findByIdAndUpdate(req.params.groupId, req.body);
+        return res.json(group);
+    } catch(err) {
+        const error = new Error('Group not found');
+        error.statusCode = 404;
+        error.errors = {message: "No group found with that id"};
+        return next(error);
+    }
 });
+
+const deleteGroup = (group) => {
+    return (
+        {message: "group is deleted"}
+    )
+}
 
 router.delete('/:groupId/users/:userId', async(req, res, next) => {
     //group destroy route for a user
+    let user;
+    try {
+        user = await User.findById(req.params.userId);
+    } catch(err) {
+        const error = new Error('User not found');
+        error.statusCode = 404;
+        error.errors = {message: "No user found with that id"};
+        return next(error);
+    }
+    try {
+        const group = await group.findById(req.params.groupId);
+        return res.json(deleteGroup(group));
+    } catch(err) {
+        const error = new Error('Group not found');
+        error.statusCode = 404;
+        error.errors = {message: "No group found with that id"};
+        return next(error);
+    }
 });
 
 router.get('/users/:userId', async(req, res, next) => {
     //group index route for a user
+    let user;
+    try {
+        user = await User.findById(req.params.userId);
+    } catch(err) {
+        const error = new Error('User not found');
+        error.statusCode = 404;
+        error.errors = {message: "No user found with that id"};
+        return next(error);
+    }
+    try {
+        const user_groups = await Group.find(req.params.userId)
+                                       .sort({createdAt: -1 })
+                                       .populate("name members budget");
+        return res.json(user_groups)
+    } catch(err) {
+        const error = new Error('Groups not found');
+        error.statusCode = 404;
+        error.errors = {message: "No groups made"};
+        return next(error);
+    }
 });
 
 router.post('/users/:userId', validateGroupInput, async(req, res, next) => {
     //group index route for a user
+    try {
+        const newGroup = new Group({
+            name: req.body.name,
+            members: req.body.members,
+            budget: req.body.budget
+          });
+        let group = await newGroup.save();
+        group = await group.populate("_id name members budget");
+        return res.json(group);
+     } catch(err) {
+        next(err);
+     }
 });
+
+router.get('/',)
 
 module.exports = router;
