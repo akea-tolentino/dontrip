24d1e0773121407d848d9f9872789d3b2084bcb6
Thomas Ly <Thomaslyy13@gmail.com>
2023-07-04 02:21:58 -0700

    double fixed merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,9 @@ import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-      <h1>don'trip</h1>   
-      <NavBar />
-    </>
-
-=======
     <NavBar>
       {/* <h1>don'trip</h1> */}
     </NavBar>
->>>>>>> main
   );
 }
 
