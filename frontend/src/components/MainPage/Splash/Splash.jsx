4acbfcdddc4113f16d62f2e07e6509cee969da36
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-07-03 23:25:46 -0700

    Create and style Splash functional component for Home

---
+++ frontend/src/components/MainPage/Splash/Splash.jsx
@@ -0,0 +1,23 @@
+import './Splash.css';
+
+export default function Splash () {
+
+    return (
+        <div className='splash-background'>
+            <article className='splash'>
+                <p className='splash-message'>
+                    Welcome to don'trip, an AI assisted web application <br/>
+                    designed to help you plan your next vacation. <br/>
+                    Sign in or sign up to get started! <br/>
+                    Alternatively, take a look at how it works with our demo.
+                    {/* <br/> Don't trip, we've got you covered. */}
+                </p>
+                <ul className='splash-buttons'>
+                    <button>Sign In</button>
+                    <button>Sign Up</button>
+                    <button>Demo</button>
+                </ul>
+            </article>
+        </div>
+    )
+}
