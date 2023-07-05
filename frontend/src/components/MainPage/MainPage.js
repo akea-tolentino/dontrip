913718ef5341d3c6190911262bd674e8190b0eb2
a-wong-8 <awong88@berkeley.edu>
2023-07-04 19:02:21 -0700

    added choose month and trip page

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -1,11 +1,14 @@
 import { useState } from "react"
+import './MainPage.css'
+import img from './snow.png'
 
 export default function MainPage() {
-    const [category, setCategory] = useState('')
+    const[category, setCategory] = useState('');
+    const[experience, setExperience] = useState('');
 
     return (
-        <>
-            <label className="choose-month">Choose a time of month
+        <form>
+            <label className="choose-month"> Choose a time of month 
                 <select onChange={(e)=>setCategory(e.target.value)}>
                     <option value={'January'}>January</option>
                     <option value={'February'}>February</option>
@@ -19,45 +22,62 @@ export default function MainPage() {
                     <option value={'October'}>October</option>
                     <option value={'November'}>November</option>
                     <option value={'December'}>December</option>
-                </select>
+                </select> and Choose a trip
             </label>
 
-            <label className="choose-experience">and Choose a trip
+            <div className="main-container">
+
+            <div className="experience-container">
+
+            <label className="experience-label">
+
                 <ul className="experience-ul">
-                    <li>
-                        Ski / Snowboard
+                    <li onClick={()=>setExperience('Ski')}>
+                        <img src={img} alt=""/>
+                            Ski / Snowboard
                     </li>
 
                     <li>
+                        <img src={img} alt=""/>
                         Lake 
                     </li>
 
                     <li>
+                        <img src={img} alt=""/>
                         Beach 
                     </li>
 
                     <li>
+                        <img src={img} alt=""/>
                         Surf
                     </li>
 
                     <li>
+                        <img src={img} alt=""/>
                         Clubbing
                     </li>
 
                     <li>
+                        <img src={img} alt=""/>
                         Backpacking
                     </li>
 
                     <li>
+                        <img src={img} alt=""/>
                         Camping
                     </li>
 
                     <li>
+                        <img src={img} alt=""/>
                         Wine tasting
                     </li>
 
                 </ul>
             </label>
-        </>
+            </div>
+
+            </div>
+
+        </form>
     )
 }
