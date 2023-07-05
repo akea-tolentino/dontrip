2786cc9d6e52246a6926ea8a8863f8e32a092683
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-07-05 11:10:22 -0700

    Fix LoginModal merge;

--- frontend/src/components/LoginModal/LoginModal.js
+++ frontend/src/components/LoginModal/LoginModal.js
@@ -5,10 +5,9 @@ import { SignUpPage } from "../NavBar/UserAuthPages/SignUpPage/SignUpPage";
 
 export default function LoginFormModal () {
 
-    const[showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false);
     const [showSignUpModal, setShowSignUpModal] = useState(false);
 
-    const [showSignUpModal, setShowSignUpModal] = useState(false);
 
     return (
         <>
@@ -17,9 +16,14 @@ export default function LoginFormModal () {
             <a id="signin" onClick={()=>setShowModal(true)}> Sign in </a>
 
             {showSignUpModal && (
-            <Modal onClose={()=> setShowSignUpModal(false)} >
-                <SignUpPage/>
-            </Modal>)}
+                <Modal onClose={()=> setShowSignUpModal(false)} >
+                    <SignUpPage/>
+                </Modal>)}
+            {showModal && (
+                <Modal onClose={()=> setShowModal(false)} >
+                    <SignInPage/>
+                </Modal>
+            )}
      </>
     )
 }
