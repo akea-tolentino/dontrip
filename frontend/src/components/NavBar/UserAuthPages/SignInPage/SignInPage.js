2cad0f2ec9a5e06a1912cef60768db14cd0449de
Thomas Ly <Thomaslyy13@gmail.com>
2023-07-05 21:00:59 -0700

    added gpt in

--- frontend/src/components/NavBar/UserAuthPages/SignInPage/SignInPage.js
+++ frontend/src/components/NavBar/UserAuthPages/SignInPage/SignInPage.js
@@ -2,9 +2,14 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import "../UserAuth.css"
 import { login } from "../../../../store/session";
+import { Redirect } from "react-router-dom";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
 
 
 export const SignInPage = () => {
+    
+    const history = useHistory();
 
     const dispatch = useDispatch();
 
@@ -20,37 +25,39 @@ export const SignInPage = () => {
             password: password
         }
 
+    
         const res = await dispatch(login(userInfo))
-        
+        return history.push("/experiences")
+
     }
 
     return (
         <>
             <div className="user-auth-container">
-                <form onSubmit={handleSignIn}>
-                    <h2 className="user-auth-title">Sign In</h2>
-                    <div className="user-auth-input-container">
-                        <label>
-                            <input
-                                type="text"
-                                value={email}
-                                placeholder="Email"
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </label>                        
-                    </div>
-                    <div className="user-auth-input-container">
-                        <label>
-                            <input
-                                type="password"
-                                value={password}
-                                placeholder="Password"
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </label>                        
-                    </div>
-                    <button className="user-auth-button" type="submit">Submit</button>
-                </form>
+                    <form onSubmit={handleSignIn}>
+                        <h2 className="user-auth-title">Sign In</h2>
+                        <div className="user-auth-input-container">
+                            <label>
+                                <input
+                                    type="text"
+                                    value={email}
+                                    placeholder="Email"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </label>                        
+                        </div>
+                        <div className="user-auth-input-container">
+                            <label>
+                                <input
+                                    type="password"
+                                    value={password}
+                                    placeholder="Password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </label>                        
+                        </div>
+                        <button className="user-auth-button" type="submit">Submit</button>
+                    </form>
             </div>
         </>
     )
