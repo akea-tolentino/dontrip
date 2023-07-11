ba4e99e4ff460706dcef04ebc4fc4ea2a558d4ea
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-07-11 15:33:56 -0700

    fix anchar link issues

--- frontend/src/components/Itinerary/Events/SelectEvents.jsx
+++ frontend/src/components/Itinerary/Events/SelectEvents.jsx
@@ -66,7 +66,7 @@ export default function SelectEvents ( { changeEvents, availableEvents, experien
                     <h3>{event.description} <button className="select-events-add-button" onClick={(e)=>handleClick(e, event)}>
                         {/* {selectedEventsDescription.includes(event.description) ? "remove" : "add"} */}select
                     </button></h3>
-                    <h3><a href={event.address} target="_blank" referrerPolicy="no-referrer" rel="noopener noreferrer" >{event.address}</a></h3>
+                    <h3><a href={`https://${event.address}`} target="_blank" referrerPolicy="no-referrer" rel="noopener noreferrer" >{event.address}</a></h3>
                 </li>)}
             </ul>
         </section>
@@ -97,7 +97,8 @@ export default function SelectEvents ( { changeEvents, availableEvents, experien
                     <h3>{event.description} <button className="select-events-add-button" onClick={(e)=>handleDelete(e, event)}>
                         Delete Event
                     </button></h3>
-                    <h3><a href={event.address} target="_blank" referrerPolicy="no-referrer" rel="noopener noreferrer" >{event.address}</a></h3>
+                    {console.log(event.address)}
+                    <h3><a href={`https://${event.address}`} target="_blank" referrerPolicy="no-referrer" rel="noopener noreferrer" >{event.address}</a></h3>
                 </li>
                 )}
             </ul>
