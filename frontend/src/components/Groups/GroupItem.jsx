a5a844a39c00e2e4c8e13c1137c44d3ed5ae0c75
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-07-11 14:37:54 -0700

    fixed the create and the delete but still need update for Groups

--- frontend/src/components/Groups/GroupItem.jsx
+++ frontend/src/components/Groups/GroupItem.jsx
@@ -1,13 +1,23 @@
 import GroupForm from "./GroupForm";
 import { useState } from 'react'
+import { deleteGroup, fetchGroups } from '../../store/groups'
+import { useDispatch } from "react-redux";
 
-export default function GroupItem ( { group } ) {
-
+export default function GroupItem ( { group, userId} ) {
+    // let groupId = group._id;
+    const dispatch = useDispatch();
     const [showForm, setShowForm] = useState(false);
 
     const handleClick = (e) => {
         e.preventDefault();
-        (showForm === false ? setShowForm(true) : setShowForm(false))
+        (showForm === false ? setShowForm(true) : setShowForm(false));
+    }
+
+    const handleDelete = (e) => {
+        e.preventDefault();
+        // debugger
+        dispatch(deleteGroup(group._id));
+        dispatch(fetchGroups(userId));
     }
 
     return (
@@ -17,10 +27,15 @@ export default function GroupItem ( { group } ) {
                 <li>members: {group.members}</li>
                 <li>budget: ${group.budget}</li>
             </ul>
-            {/* <button onClick={handleClick}>edit</button>
+            <button onClick={handleClick}>Edit Group</button>
             {showForm && (
-                <GroupForm groupId={group._id} />
-                )} */}
+                <GroupForm userId={userId} groupId={group._id} />
+                )}
+            <button onClick={handleDelete}>Delete Group</button>
+            {showForm && (
+                <GroupForm userId={userId} groupId={group._id} />
+                )}
+
         </article>
     )
 }
