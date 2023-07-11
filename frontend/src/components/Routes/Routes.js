3c803b330c83b2452dc995b242824618e0b8b5ad
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-07-11 15:46:40 -0700

    Route logged in users to users show page from home

--- frontend/src/components/Routes/Routes.js
+++ frontend/src/components/Routes/Routes.js
@@ -7,6 +7,7 @@ import { useSelector } from 'react-redux';
 
 export const AuthRoute = ({ component: Component, path, exact }) => {
   const loggedIn = useSelector(state => !!state.session.user);
+  const user = useSelector(state => state.session.user);
 
 
   return (
@@ -14,7 +15,7 @@ export const AuthRoute = ({ component: Component, path, exact }) => {
       !loggedIn ? (
         <Component {...props} />
       ) : (
-        <Redirect to="/" />
+        <Redirect to={`/users/${user._id}/trips`} />
       )
     )} />
   );
