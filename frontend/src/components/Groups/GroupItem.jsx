55878fc83a7b6cef592f6f4925f26303cc930f84
a-wong-8 <awong88@berkeley.edu>
2023-07-11 20:19:16 -0700

    fixed update group

--- frontend/src/components/Groups/GroupItem.jsx
+++ frontend/src/components/Groups/GroupItem.jsx
@@ -1,40 +1,47 @@
 import GroupForm from "./GroupForm";
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { deleteGroup, fetchGroups } from '../../store/groups'
 import { useDispatch } from "react-redux";
 
 export default function GroupItem ( { group, userId} ) {
-    // let groupId = group._id;
     const dispatch = useDispatch();
     const [showForm, setShowForm] = useState(false);
 
     const handleClick = (e) => {
         e.preventDefault();
-        (showForm === false ? setShowForm(true) : setShowForm(false));
+        setShowForm(false);
+        (!showForm) ? setShowForm(true) : setShowForm(false);
     }
 
     const handleDelete = (e) => {
         e.preventDefault();
-        // debugger
         dispatch(deleteGroup(group._id));
         dispatch(fetchGroups(userId));
     }
 
     return (
         <article>
+
             <ul className="group-item-wrapper">
                 <li>{group.name}</li>
                 <li>members: {group.members}</li>
                 <li>budget: ${group.budget}</li>
             </ul>
-            <button onClick={handleClick}>Edit Group</button>
+
+            <button onClick={handleClick}>
+                Edit Group
+            </button>
+
             {showForm && (
-                <GroupForm userId={userId} groupId={group._id} />
+                <div className="edit-form-dropdown">
+                    <div className="edit-x" onClick={()=>setShowForm(false)}>X</div>
+                    <GroupForm userId={userId} groupId={group._id}/>
+                </div>
                 )}
-            <button onClick={handleDelete}>Delete Group</button>
-            {showForm && (
-                <GroupForm userId={userId} groupId={group._id} />
-                )}
+
+            <button onClick={handleDelete}>
+                Delete Group
+            </button>
 
         </article>
     )
